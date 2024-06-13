b6ee8ca2692ad88aa73b99c1a2897c5215966110
Clarity Shimoniak <clarity.shimoniak@email.ucr.edu>
2024-06-12 22:38:27 -0700

    Oh yeah it's all coming together
    (untested but synthesizes)

---
+++ verilog/minimobilenet.sv
@@ -0,0 +1,56 @@
+`timescale 1ns / 1ps
+
+// Convolutional Layer
+// More output channels can be accomplished with more instances of this module
+module minimobilenet #(
+	// Input size, assumed to be square
+	parameter INPUT_SIZE = 32,
+	// Input image channels/features
+	parameter INPUT_CHANNELS = 3,
+	// Bits per pixel
+	parameter PX_SIZE = 8,
+	// Number of classes
+	parameter OUTPUT_CHANNELS = 10
+) (
+	input wire
+		[INPUT_SIZE-1:0][INPUT_SIZE-1:0]
+		[INPUT_CHANNELS-1:0]
+		[PX_SIZE-1:0] img_in,
+	output reg
+		[OUTPUT_CHANNELS-1:0]
+		[PX_SIZE-1:0] img_out
+);
+	genvar i, j;
+	localparam CONV_OUT_CHANNELS = 6;
+	localparam CONV_KERNEL_SIZE = 3;
+	localparam CONV_OUT_SIZE = INPUT_SIZE - (CONV_KERNEL_SIZE-1);
+	wire[CONV_OUT_CHANNELS-1:0]
+		[CONV_OUT_SIZE-1:0][CONV_OUT_SIZE-1:0]
+		[PX_SIZE-1:0] relu_conv_out;
+
+	generate
+		for (i = 0; i < CONV_OUT_CHANNELS; i += 1) begin
+			wire[CONV_OUT_SIZE-1:0][CONV_OUT_SIZE-1:0]
+				[INPUT_CHANNELS-1:0]
+				[PX_SIZE-1:0] raw_conv_out;
+			conv_layer #(
+				.INPUT_SIZE(INPUT_SIZE),
+				.INPUT_CHANNELS(INPUT_CHANNELS),
+				.KERNEL_SIZE(3),
+				.PX_SIZE(8)
+			) layer (
+				.img_in(img_in),
+				.kernel(kernels[i]),
+				.img_out(raw_conv_out)
+			);
+			relu_layer #(
+				.INPUT_SIZE(CONV_OUT_SIZE),
+				.INPUT_CHANNELS(1),
+				.PX_SIZE(PX_SIZE)
+			) relu (
+				.img_in(raw_conv_out),
+				.img_out(relu_conv_out[i])
+			);
+		end
+	endgenerate
+endmodule
---
+++ verilog/relu_layer.sv
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+
+// Rectifier Layer
+module relu_layer #(
+	// Input size, assumed to be square
+	parameter INPUT_SIZE = 5,
+	// Input image channels/features
+	parameter INPUT_CHANNELS = 3,
+	// Bits per pixel
+	parameter PX_SIZE = 8
+) (
+	input wire
+		[INPUT_SIZE-1:0][INPUT_SIZE-1:0]
+		[INPUT_CHANNELS-1:0]
+		[PX_SIZE-1:0] img_in,
+	output reg
+		[INPUT_SIZE-1:0][INPUT_SIZE-1:0]
+		[INPUT_CHANNELS-1:0]
+		[PX_SIZE-1:0] img_out
+);
+	genvar x, y;
+
+	generate
+		for (x = 0; x < INPUT_SIZE; x += 1) begin
+			for (y = 0; y < INPUT_SIZE; y += 1) begin
+				for (c = 0; c < INPUT_SIZE; c += 1) begin
+					always @(img_in[x][y][c]) begin
+						if (img_in[x][y][c] > 0) begin
+							img_out[x][y][c] = img_in[x][y][c];
+						end else begin
+							img_out[x][y][c] = 0;
+						end
+					end
+				end
+			end
+		end
+	endgenerate
+endmodule
